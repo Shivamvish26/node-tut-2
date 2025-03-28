6b78708b38b452c9ea7192d8ae4e7cad774a0741
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-03-28 18:08:59 +0530

    coded added

--- index.js
+++ index.js
@@ -5,11 +5,32 @@ const main = async () => {
   await mongoose.connect("mongodb://localhost:27017/e-commerce");
   const ShopSchema = new mongoose.Schema({
     name: String,
+    Age: Number,
   });
-  const ShopSchema1 = mongoose.model("shop", ShopSchema);
-  let data = new ShopSchema1({ name: "Raj" });
+  const ShopSchema1 = mongoose.model("shop", ShopSchema, "shop");
+  let data = new ShopSchema1({ name: "shivam2", Age: 20 });
   let result = await data.save();
   console.log(result);
 };
 
 main();
+
+// mongoose is a ODM library for MongoDB and Node.js
+// const mongoose = require("mongoose");
+
+// const main = async () => {
+//   await mongoose.connect("mongodb://localhost:27017/e-commerce");
+//   const ShopSchema = new mongoose.Schema({
+//     name: String,
+//     Age: Number,
+//   });
+//   const ShopSchema1 = mongoose.model("shop", ShopSchema, "shop");
+//   //   let data = new ShopSchema1({ name: "shivam2", Age: 20 });
+//   let result = await ShopSchema1.create({
+//     name: "shivam6",
+//     Age: 20,
+//   });
+//   console.log(result);
+// };
+
+// main();
