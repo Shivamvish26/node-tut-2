d1691093a13ef52b8d885941640620108a7010e2
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-04-02 12:33:03 +0530

    node api done satrted with my sql server

--- index2.js
+++ index2.js
@@ -1,1 +1,32 @@
+//Event and event Emitter
 const express = require("express");
+const EventEmitter = require("events"); //It is a class
+const app = express();
+
+const event = new EventEmitter(); //Creating an instance of EventEmitter class
+
+let count = 0;
+
+event.on("Count API Called", () => {
+  count++;
+  console.log("Count API Called Event Triggered", count);
+});
+
+app.get("/", (req, resp) => {
+  resp.send("Api Called");
+  event.emit("Count API Called");
+});
+
+app.get("/search", (req, resp) => {
+  resp.send("Search Api Called");
+  event.emit("Count API Called");
+});
+
+app.get("/update", (req, resp) => {
+  resp.send("Update Api Called");
+  event.emit("Count API Called");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
